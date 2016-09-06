86e2d3b2782ed4320a116122bbe1229cd9c77e70
d119322 <d119322@LI142>
2016-09-05 22:36:50 -0300

    Adicionado o arquivo LeDados. Objetivo: Ler os arquivos de microregioes e dados dos procons para transportar nas classes. Ajustados e refatorados as classes com nomes padronizados. Ajustados os nomes dos atributos conforme os dados dos arquivos. É preciso ajustar o retorno das linhas do arquivo para os objetos.

---
+++ Procon/src/Classes/Regiao.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Classes;
+  import java.util.ArrayList;
+  import java.util.List;
+/**
+ *
+ * @author d119322
+ */
+
+
+public class Regiao {
+    
+    List<Cidade> cidade = new ArrayList();
+    private String microregiao;
+    private Cidade cidades;
+    
+    public Regiao(){
+    }
+    
+    public void Imprimir(){
+             
+    }
+    
+            
+}
+
+//Arquivo de cidades do Brasil e suas microregiões disponivel em 
+//ftp://geoftp.ibge.gov.br/organizacao_do_territorio/estrutura_territorial/divisao_territorial/2015/dtb_2015.zip
